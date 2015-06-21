51c2bc00e3c86c001d2a0cbc62bad98aa60130eb
sprasain1 <sabal.prasain@gmail.com>
2015-06-20 22:40:01 -0700

    empty_viewModel_created

---
+++ ProfilePort/App/app.js
@@ -0,0 +1,29 @@
+﻿(function () {
+
+    // construct the application.
+    var app = angular.module('app', ['ngRoute', 'ngAnimate', 'toastr', 'ngCookies', 'ui.bootstrap', 'ui.mask', 'kendo.directives', 'chieffancypants.loadingBar', 'xtForm']);
+   
+    app.run(['$rootScope', '$location', 'toastr', 'menuService',
+    function ($rootScope, $location, toastr, menuService) {
+
+        //Client-side security
+        $rootScope.$on("$routeChangeStart", function () {
+            menuService.clearSecondarMenu();
+        });
+
+        $rootScope.$on("httpResponseError", function (event, args) {
+            if (args.response.status === 0) {
+                // Do not pop during demo
+                toastr.error("It looks like there has been a disruption in service." +
+                "  Please check your internet connection and try refreshing your browser.", "Connection Error " + args.response.status);
+            }
+            else if (args.response.status === 400) {
+                // Do nothing when server side errors occur    
+            }
+            else {
+                toastr.error("An error occurred while loading the web page.", "Connection Error " + args.response.status);
+            }
+        });
+    }]);
+}());
+
---
+++ ProfilePort/App/config.js
@@ -0,0 +1,80 @@
+﻿
+(function () {
+    // configure the routing for the site.
+    angular.module('app').config(['$routeProvider', '$locationProvider', '$httpProvider', '$logProvider', 'siteConfig',
+        function ($routeProvider, $locationProvider, $httpProvider, $logProvider, siteConfig) {
+
+            $logProvider.debugEnabled(true);
+
+            $locationProvider.html5Mode(false);
+
+            var authResolver = function (fn) {
+                return {
+                    load: ['$q', '$http', function ($q, $http) {
+                        var deferred = $q.defer();
+                        $http.get('/auth/session')
+                            .success(function (results) {
+                                if (fn == null) {
+                                    deferred.resolve(results);
+                                } else {
+                                    deferred.resolve(fn(results));
+                                }
+                            })
+                            .error(function () {
+
+                                deferred.reject(false);
+                            });
+                        return deferred.promise;
+                    }]
+                }
+            };
+
+            // Routing structure
+            $routeProvider
+              
+                .when('/404', { templateUrl: 'App/Common/Partials/404.html', controller: '404Ctrl', resolve: authResolver() })
+                .when('/dashboard', { templateUrl: 'App/Dashboard/Partials/dashboard.html', controller: 'DashboardCtrl', resolve: authResolver() })
+                .when('/profile/:id?', { templateUrl: 'App/Profile/Partials/Profile.html', controller: 'ProfileCtrl', resolve: authResolver() })
+                 .when('/message/:id?', { templateUrl: 'App/Message/Partials/Message.html', controller: 'ProfileCtrl', resolve: authResolver() })
+
+                // reports
+                .when('/', { redirectTo: 'login' })
+                .otherwise({ redirectTo: '404' });
+
+            // Http Settings
+            $httpProvider.defaults.withCredentials = true;
+
+            // alternatively, register the interceptor via an anonymous factory
+            $httpProvider.interceptors.push(['$location', '$q', '$rootScope', function ($location, $q, $rootScope) {
+                return {
+                    'request': function (config) {
+
+                        return config;
+                    },
+
+                    'response': function (response) {
+                        return response;
+                    },
+                    'responseError': function (response) {
+                        // request was unauthenticated, redirect away.
+                        if (response.status === 401) {
+
+                            if ($location.$$path.indexOf("/login") === 0) {
+                                // do nothing when you're on the login page, without this line the site will lockup
+                            }
+                            else {
+                                var expectedUrl = encodeURIComponent(window.location.hash.replace("#", ""));
+                                $location.path('/login/' + expectedUrl);
+                            }
+                            return $q.reject(response);
+                        }
+                        $rootScope.$broadcast('httpResponseError', { response: response });
+
+                        return $q.reject(response);
+                    }
+                };
+            }]);
+
+
+        }]);
+}());
