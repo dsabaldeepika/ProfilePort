dc310471f22b4135b224411225b8b1548a1079f6
sprasain1 <sabal.prasain@gmail.com>
2015-06-21 18:00:08 -0700

    tra

---
+++ ProfilePort/App/Dashboard/Controllers/DashboardCtrl.js
@@ -0,0 +1,10 @@
+﻿/**
+ * Created by devin.delapp on 9/15/2014.
+ */
+
+(function () {
+
+    angular.module('app').controller('co404Ctrl', ['$scope', function ($scope) {
+        $scope.page.title = '404 Page Not Found';
+    }]);
+}());
