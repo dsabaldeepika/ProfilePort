7e374863e02299f962e5fdd8385e7faccc976b17
sprasain1 <sabal.prasain@gmail.com>
2015-06-26 11:17:40 -0700

    sa

--- ClientPort/App/notes/services/notesService.js
+++ ClientPort/App/notes/services/notesService.js
@@ -6,10 +6,11 @@
           
                 factory = {};
 
+                var URL = "https://moviez.firebaseio.com/";
             //Save notes for a notes 
-            factory.getNotes = function (model) {
+            factory.getNotes = function () {
 
-                return $http.get('/notes', model)
+                return $http.get(URL)
                     .success(function (data, status, headers, config) {
 
                    
@@ -21,6 +22,25 @@
                         return false;
                     });
             }
+        
+
+            //Save notes for a notes 
+            factory.postNotes = function (note) {
+
+                return $http.post(URL+note+'.json')
+                    .success(function (data, status, headers, config) {
+
+
+                        return true;
+                    })
+                    .error(function (data, status, headers, config) {
+
+
+                        return false;
+                    });
+            }
+
+
 
 
            
